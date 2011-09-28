5e5a2b89e93bc603f5bc879ba472ac8e58719862
Mike Bannister <mikebannister@gmail.com>
2011-09-28 14:57:54 -0400

    fixed migration mispaste

--- db/migrate/20110925191520_create_foos.rb
+++ db/migrate/20110925191520_create_foos.rb
@@ -1,8 +1,8 @@
 class CreateFoos < ActiveRecord::Migration
   def change
     create_table :foos do |t|
-      t.datetime_select(:foo, :foo_date)
-      
+      t.date :foo_date
+
       t.timestamps
     end
   end
